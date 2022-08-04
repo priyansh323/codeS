6e5e0283a980805ab524233001244546db94dbc2
priyanshu <priyanshuchauhan323@gmail.com>
2022-08-04 12:49:10 +0530

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+class abc{
+
+    constructor(prop1, prop2){
+        this.prop1 = prop1;
+        this.prop2 = prop2;
+    }
+     function(){
+        console.log('x');
+     }
+}
+
+let obj1 = new abc('x','y')
+ console.log(obj1);
