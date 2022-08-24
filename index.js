0d44c0f7b06acc4c21585f550f2477918d0124b2
PRIYANSHU CHAUHAN <53870271+priyansh323@users.noreply.github.com>
2022-08-24 16:11:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ class abc{
         this.prop1 = prop1;
         this.prop2 = prop2;
     }
-     function(){
-        console.log('x');
+     abc1(){
+        console.log('x',this.prop1, this.prop2);
+     }
+
+     abc2(){
+        console.log('y', this.prop1,this.prop2)
      }
 }
-
+ 
 let obj1 = new abc('x','y')
- console.log(obj1);
+console.log(obj1.abc1());
+
+let obj2 = new abc('pq','rs')
+console.log(obj2.abc2());
